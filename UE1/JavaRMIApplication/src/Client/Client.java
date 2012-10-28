dc1921e1649c10875ee71aa3762e35df0fe9f912
Nepetalactone <tobias.meusburger@students.fhv.at>
2012-10-29 00:34:55 +0100

    Finished Assignment
    Signed-off-by: Nepetalactone <tobias.meusburger@students.fhv.at>

--- UE1/JavaRMIApplication/src/Client/Client.java
+++ UE1/JavaRMIApplication/src/Client/Client.java
@@ -5,6 +5,9 @@
 package Client;
 
 import Interface.ISquare;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.AccessException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -12,26 +15,39 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import sun.net.util.IPAddressUtil;
 
 /**
  *
  * @author Tobias
  */
 public class Client {
+
     private static ISquare stub = null;
-    private Client(){};
+
+    private Client() {
+    }
+
+    ;
     
-    public static void main(String[] args){
-        
-        try{
-        
-        }catch(Exception e){
-            System.out.println("Malformed input");
+    public static void main(String[] args) throws IOException {
+
+        /*
+         * try{
+         *
+         * }catch(Exception e){ System.out.println("Malformed input");
         }
-        
-        try {
-            Registry registry = LocateRegistry.getRegistry("localhost", 33333);
-            stub = (ISquare)registry.lookup("Square");
+         */
+
+        try {//Parse IP-address
+            String address = args[0];   
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            while (!IPAddressUtil.isIPv4LiteralAddress(address)) {
+                System.out.print("Malformed input. Enter proper IPv4-address");
+                address = in.readLine();
+            }
+            Registry registry = LocateRegistry.getRegistry(address, 33333);
+            stub = (ISquare) registry.lookup("Square");
         } catch (NotBoundException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         } catch (AccessException ex) {
@@ -39,9 +55,31 @@ public class Client {
         } catch (RemoteException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
-        try {
-            int number = Integer.parseInt(args[0]);
-            System.out.println(stub.calculateSquare(number));
+        
+        try {//Parse number
+            boolean parse = false;
+            int number = 0;
+            String tempNumber;
+            
+            try{
+                number = Integer.parseInt(args[1]);
+            } catch (Exception e){
+            
+            System.out.println("Malformed Input. Enter proper integer");
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            tempNumber = in.readLine();
+            
+            while (parse == false) {
+                try {
+                    number = Integer.parseInt(tempNumber);
+                    parse = true;
+                } catch (Exception ex) {
+                        System.out.println("Malformed Input. Enter proper integer");
+                        tempNumber = in.readLine();
+                    }
+                }
+            }
+            System.out.println(stub.calculateSquare(number)); //Send number to server, output result
         } catch (RemoteException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
---
+++ UE1/Readme.txt
@@ -0,0 +1,2 @@
+1. Server starten
+2. Client starten mit Argumenten IP-Adresse und Nummer die quadriert werden soll
